6cc34c5ad071a390d2dc85c8b126094ed92cc46b
DevLimk1 <kyenwon2@naver.com>
2020-09-02 15:35:09 +0900

    JPA , DB 설정, Test Code작성

---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,39 @@
+package jpabook.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class) //junit 한테 Spring 관련된 것을 test 할 거다 라는 어노테이션
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(false)
+    public void testMember() throws Exception{
+        //given
+        Member member=new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
+
+        //then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member);
+        //영속성컨텍스트에서 식별자가 같으면 같은 엔터티로 인식
+        //1차캐시라고 불리는 곳에서 가지고옴
+        System.out.println("findMember==member : "+ (findMember==member));
+
+    }
+}
